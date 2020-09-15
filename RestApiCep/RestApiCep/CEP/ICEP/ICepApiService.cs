dc014b2ccf37efb17c51265316c28e651e7e8750
Rafael Buttignon <rafael.buttignon@programmers.com.br>
2020-09-15 15:06:51 -0300

    Rest Api Search Cep

---
+++ RestApiCep/RestApiCep/CEP/ICEP/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace RestApiCep
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ RestApiCep/RestApiCep/Program.cs
@@ -0,0 +1,35 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace RestApiCep
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+                Console.WriteLine("Informe seu cep: ");
+
+                var cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"" +
+                    $"\nLogradouro: {address.Logradouro}" +
+                    $"\nBairro: {address.Bairro}" +
+                    $"\nComplemento: {address.Complemento}" +
+                    $"\nCidade: {address.Localidade}" +
+                    $"\nUf: {address.Uf}");
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+        }
+    }
+}
